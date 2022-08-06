597d856ac0f26b95085271028038aee8ddc7b09e
Sourav Sahoo <jackysourav1994@gmail.com>
2022-08-06 09:16:55 +0530

    commit again 6/08

--- Competative_Programming/src/Others/quest.java
+++ Competative_Programming/src/Others/quest.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class quest {
 
 	public static void main(String[] args) {
-		
-		int arr[] = {10,20,30,40,50};
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
 		changeArray(arr,arr.length);
 		for(int i = 0; i< arr.length;i++)
 			System.out.println(arr[i]);
