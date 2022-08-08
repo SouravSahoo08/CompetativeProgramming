98bd9d86aa5766886b4155607f8b63600dd34ba4
Sourav Sahoo <jackysourav1994@gmail.com>
2022-08-08 22:56:05 +0530

    commit 8/8/22

--- Competative_Programming/src/Others/Unique_Identification_number.java
+++ Competative_Programming/src/Others/Unique_Identification_number.java
@@ -5,39 +5,48 @@ import java.util.Scanner;
 public class Unique_Identification_number {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		//String uin = sc.next();
-		int x = (char)'9' - (char)'0';
-		System.out.println(x);
-		//validate_Uin(uin);
+		/*
+		 * int x = 9; System.out.println(Character.forDigit(x, 10));
+		 */
+
+		String uin = sc.next();
+		System.out.println(validate_Uin(uin));
 
 	}
 
-	private static void validate_Uin(String uin) {
+	private static boolean validate_Uin(String uin) {
 		char[] uin_char = uin.toCharArray();
-		
-		for(int i = 0;i<uin_char.length;i+=2) {
+
+		for (int i = 0; i < uin_char.length; i += 2) {
 			int n = Character.getNumericValue(uin_char[i]);
-			int triple = n*3;
-			if(triple>9) {
-				uin_char[i] = (char) ((char)getSum(triple) - (char)'0'); 
-				
+			int triple = n * 3;
+			if (triple > 9) {
+				uin_char[i] = Character.forDigit(getSum(triple), 10);
+			} else {
+				uin_char[i] = Character.forDigit(triple, 10);
 			}
 		}
-		
-		
+
+		int sum = 0;
+		for (char i : uin_char) {
+			sum += Character.getNumericValue(i);
+		}
+
+		if (sum % 10 == 0)
+			return true;
+		return false;
 	}
 
 	private static int getSum(int n) {
-		
+
 		int sum = 0;
-		while(n!=0) {
-			sum += n%10;
-			n/=10;
+		while (n != 0) {
+			sum += n % 10;
+			n /= 10;
 		}
 		return sum;
 	}
 
-	
 }
